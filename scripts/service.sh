612b54e021a3b6d976ecdf4e12d110eb97f04aa2
AdamMiltonBarker <adammiltonbarker@eu.techbubbletechnologies.com>
2021-06-19 14:28:02 +0200

    1.0.1 Fix

--- scripts/service.sh
+++ scripts/service.sh
@@ -6,24 +6,24 @@ read -p "? This script will install the HIAS MQTT IoT Agent service on your devi
 
 if [ "$cmsg" = "Y" -o "$cmsg" = "y" ]; then
 	echo "- Installing HIAS MQTT IoT Agent service"
-	sudo touch /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "[Unit]" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "Description=HIAS MQTT IoT Agent service" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "After=multi-user.target" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "After=HIASCDI.service" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "[Service]" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "User=$USER" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "Type=simple" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "Restart=on-failure" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "ExecStart=/usr/bin/python3 /home/$USER/HIAS/components/agents/mqtt/agent.py" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "[Install]" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
-	echo "" | sudo tee -a /lib/systemd/system/HIAS-MQTT-Agent.service
+	sudo touch /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "[Unit]" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "Description=HIAS MQTT IoT Agent service" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "After=multi-user.target" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "After=HIASCDI.service" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "[Service]" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "User=$USER" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "Type=simple" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "Restart=on-failure" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "ExecStart=/usr/bin/python3 /home/$USER/HIAS/components/agents/mqtt/agent.py" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "[Install]" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "WantedBy=multi-user.target" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
+	echo "" | sudo tee -a /lib/systemd/system/HIAS-MQTT-IoT-Agent.service
 
-	sudo systemctl enable HIAS-MQTT-Agent.service
-	sudo systemctl start HIAS-MQTT-Agent.service
+	sudo systemctl enable HIAS-MQTT-IoT-Agent.service
+	sudo systemctl start HIAS-MQTT-IoT-Agent.service
 
 	echo "- Installed HIAS MQTT IoT Agent service!";
 	exit 0
